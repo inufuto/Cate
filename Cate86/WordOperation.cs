f8730105a0138c7c5b7a9363bef7803f4ab816de
いぬふと <mxb03523@nifty.com>
2022-07-10 13:49:28 +0900

    Cate86,99,87を一旦削除

--- Cate86/WordOperation.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Inu.Cate.I8086
-{
-    internal class WordOperation : Cate.WordOperation
-    {
-        public override List<Cate.WordRegister> Registers => WordRegister.Registers;
-    }
-}
