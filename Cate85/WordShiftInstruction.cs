1b46f1cc68e7b1a31356fe2c38e898becde82e54
いぬふと <mxb03523@nifty.com>
2024-11-03 11:38:10 +0900

    [SM85]ワードシフト修正

--- Cate85/WordShiftInstruction.cs
+++ Cate85/WordShiftInstruction.cs
@@ -12,44 +12,67 @@ internal class WordShiftInstruction(
 {
     protected override void ShiftConstant(int count)
     {
-        Action<Cate.WordRegister> action = OperatorId switch
-        {
-            Keyword.ShiftLeft => register =>
-            {
-                WriteLine("\tsll\t" + register.Low);
-                WriteLine("\trlc\t" + register.High);
+        if (count == 8) {
+            if (OperatorId == Keyword.ShiftRight && !((IntegerType)LeftOperand.Type).Signed) {
+                using var reservation = WordOperation.ReserveAnyRegister(this, DestinationOperand);
+                var wordRegister = reservation.WordRegister;
+                wordRegister.Load(this, LeftOperand);
+                Debug.Assert(wordRegister is { Low: not null, High: not null });
+                wordRegister.Low.CopyFrom(this, wordRegister.High);
+                wordRegister.High.LoadConstant(this, 0);
+                return;
             }
-            ,
-            Keyword.ShiftRight when ((IntegerType)LeftOperand.Type).Signed => register =>
-            {
-                WriteLine("\tsra\t" + register.High);
-                WriteLine("\trrc\t" + register.Low);
+            if (OperatorId == Keyword.ShiftLeft) {
+                using var reservation = WordOperation.ReserveAnyRegister(this, DestinationOperand);
+                var wordRegister = reservation.WordRegister;
+                wordRegister.Load(this, LeftOperand);
+                Debug.Assert(wordRegister is { Low: not null, High: not null });
+                wordRegister.High.CopyFrom(this, wordRegister.Low);
+                wordRegister.Low.LoadConstant(this, 0);
+                return;
             }
-            ,
-            Keyword.ShiftRight => register =>
-            {
-                WriteLine("\tsrl\t" + register.High);
-                WriteLine("\trrc\t" + register.Low);
-            }
-            ,
-            _ => throw new NotImplementedException()
-        };
-
-        if (DestinationOperand.Register is WordRegister wordRegister) {
-            ViaRegister(wordRegister);
-            return;
         }
-        using var reservation = WordOperation.ReserveAnyRegister(this, LeftOperand);
-        ViaRegister(reservation.WordRegister);
-        return;
-
-        void ViaRegister(Cate.WordRegister register)
         {
-            register.Load(this, LeftOperand);
-            for (var i = 0; i < count; ++i) {
-                action(register);
+
+            Action<Cate.WordRegister> action = OperatorId switch
+            {
+                Keyword.ShiftLeft => register =>
+                {
+                    WriteLine("\tsll\t" + register.Low);
+                    WriteLine("\trlc\t" + register.High);
+                }
+                ,
+                Keyword.ShiftRight when ((IntegerType)LeftOperand.Type).Signed => register =>
+                {
+                    WriteLine("\tsra\t" + register.High);
+                    WriteLine("\trrc\t" + register.Low);
+                }
+                ,
+                Keyword.ShiftRight => register =>
+                {
+                    WriteLine("\tsrl\t" + register.High);
+                    WriteLine("\trrc\t" + register.Low);
+                }
+                ,
+                _ => throw new NotImplementedException()
+            };
+            if (DestinationOperand.Register is WordRegister wordRegister) {
+                ViaRegister(wordRegister);
+                return;
             }
-            register.Store(this, DestinationOperand);
+            using var reservation = WordOperation.ReserveAnyRegister(this, LeftOperand);
+            ViaRegister(reservation.WordRegister);
+            return;
+
+            void ViaRegister(Cate.WordRegister register)
+            {
+                register.Load(this, LeftOperand);
+                for (var i = 0; i < count; ++i) {
+                    action(register);
+                }
+
+                register.Store(this, DestinationOperand);
+            }
         }
     }
 
@@ -65,40 +88,25 @@ internal class WordShiftInstruction(
         };
         var valueRegister = WordRegister.FromAddress(0);
         var counterRegister = ByteRegister.FromAddress(3);
-        if (counterOperand.Register == null || !counterOperand.Register.Conflicts(valueRegister)) {
-            using (WordOperation.ReserveRegister(this, valueRegister, LeftOperand)) {
-                valueRegister.Load(this, LeftOperand);
-                using (ByteOperation.ReserveRegister(this, counterRegister)) {
-                    counterRegister.Load(this, counterOperand);
-                    Compiler.CallExternal(this, functionName);
-                }
-                AddChanged(valueRegister);
-                RemoveRegisterAssignment(valueRegister);
-                valueRegister.Store(this, DestinationOperand);
-            }
-        }
-        else {
-            if (DestinationOperand.Register != null && DestinationOperand.Register.Conflicts(counterRegister)) {
-                ViaCounter();
-            }
-            else {
-                using (ByteOperation.ReserveRegister(this, counterRegister)) {
-                    ViaCounter();
-                }
-            }
-
-            void ViaCounter()
-            {
+        using (WordOperation.ReserveRegister(this, valueRegister, LeftOperand)) {
+            valueRegister.Load(this, LeftOperand);
+            using (ByteOperation.ReserveRegister(this, counterRegister)) {
                 counterRegister.Load(this, counterOperand);
-                using (WordOperation.ReserveRegister(this, valueRegister, LeftOperand)) {
-                    valueRegister.Load(this, LeftOperand);
-                    Compiler.CallExternal(this, functionName);
-                    AddChanged(valueRegister);
-                    RemoveRegisterAssignment(valueRegister);
-                    valueRegister.Store(this, DestinationOperand);
-                }
+                Compiler.CallExternal(this, functionName);
+            }
+            AddChanged(valueRegister);
+            RemoveRegisterAssignment(valueRegister);
+            valueRegister.Store(this, DestinationOperand);
+        }
+    }
+
+    public override int? RegisterAdaptability(Variable variable, Register register)
+    {
+        if (RightOperand is VariableOperand variableOperand && variableOperand.Variable.Equals(variable)) {
+            if (register.Conflicts(ByteRegister.FromAddress(3))) {
+                return 1;
             }
         }
-
+        return base.RegisterAdaptability(variable, register);
     }
 }
